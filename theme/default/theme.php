d57a40d090c10223265bf1e6caaa5dbf11c305c8
Guy Morin <guy.morin1@gmail.com>
2020-03-27 18:00:08 +0100

    Include page for the theme

---
+++ theme/default/theme.php
@@ -0,0 +1,12 @@
+<?php 
+echo "<link rel='stylesheet' type='text/css' media='all' ";
+echo "href='theme/default/style.css' />\n";
+
+// Icons
+$icon_championship="&#127942;";
+$icon_homepage="&#127968;";
+$icon_matchday="&#x1f4c6;";
+$icon_player="&#127939;";
+$icon_season="&#127937;";
+$icon_team="&#127933;";
+?>
