76586706a41385068063782128f9c6fb1124bcf0
Guy Morin <guy.morin1@gmail.com>
2020-04-02 16:21:56 +0200

    Fix the autoloader register call

--- class/Autoloader.php
+++ class/Autoloader.php
@@ -12,11 +12,11 @@ class Autoloader
     static function register(){
         spl_autoload_register(array(__CLASS__,"autoload"));
     }
-    static function autoload(){
-        if (strpos($class,__NAMESPACE__ . "\\") == 0){
-            $class = str_replace(__NAMESPACE__ . "\\", "/", $class);
-            $class = str_replace("\\", "/", $class);
-            require(__DIR__ . "/".$class_name.".php");            
+    static function autoload($class_name){
+        if (strpos($class_name,__NAMESPACE__ . "\\") == 0){
+            $class_name = str_replace(__NAMESPACE__ . "\\", "/", $class_name);
+            $class_name = str_replace("\\", "/", $class_name);
+            require(__DIR__ . "/" .$class_name.".php");            
         }
 
     }
