db639249375529045316a7bc504f40e3e3f96499
Guy Morin <guy.morin1@gmail.com>
2020-04-10 00:04:05 +0200

    Team select is now in the Team class

--- pages/team_select.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-        echo "  		<option value='0'>...</option>\n";
-        while ($data = $response->fetch(PDO::FETCH_OBJ))
-        {
-            echo "  		<option value='".$data->id_team."'>".$data->name."</option>\n";
-        }
-?>
